8ece4ccef2a60df57650a21f7262c8df2b3fda3b
InerstIO <inerstio@gmail.com>
2018-10-07 20:54:43 -0700

    fix readRecord

--- rbf/rbfm.cc
+++ rbf/rbfm.cc
@@ -208,15 +208,13 @@ RC RecordBasedFileManager::readRecord(FileHandle &fileHandle, const vector<Attri
     void *page = malloc(PAGE_SIZE);
     int rc = fileHandle.readPage(rid.pageNum, page);
     if (rc) {
-        cout << rid.pageNum << rid.slotNum<<endl;
         return rc;
     }
     SlotDir slotDir;
     memcpy(&slotDir, (char *)page + PAGE_SIZE - 2 * sizeof(int) - rid.slotNum * sizeof(SlotDir), sizeof(SlotDir));
     char *record = new char[slotDir.length];
     memcpy((char *)record, (char *)page + slotDir.offset, slotDir.length);
-    int length;// No need???????????????????????????????????????????????????????
-    data = record2data(record, recordDescriptor, length);
+    record2data(record, recordDescriptor, data);
     delete[] record;
     return 0;
 }
